ec7b702ae68495bd031cae24bfcd686d5baa1b25
Alex <akuzin20@gmail.com>
2019-10-05 12:22:48 +0300

    init v37  +editMode for Status in ProfileInfo Component

--- src/components/profilePage/profileInfo/ProfileInfo.js
+++ src/components/profilePage/profileInfo/ProfileInfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from "../../../common/Preloader";
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
     if (!props.profile){
@@ -11,11 +12,11 @@ const ProfileInfo = (props) => {
         <div>
             <div className={s.profDescription}>
                 <div>
-                    <img className={s.avatar} src="https://avatarko.ru/img/kartinka/32/Star_Wars_pistolet_31680.jpg" align="top" hspace="10"/>Princess Leia
+                    {/*<img className={s.avatar} src="https://avatarko.ru/img/kartinka/32/Star_Wars_pistolet_31680.jpg" align="top" hspace="10"/>Princess Leia*/}
                 </div>
                 <div>
                     <img src={props.profile.photos.large} alt=""/>
-                    description
+                    <ProfileStatus status={'Hello!!!'}/>
                 </div>
             </div>
         </div>
